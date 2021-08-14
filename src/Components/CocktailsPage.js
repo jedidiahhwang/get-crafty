8a2a2ed54c0b1c5c6f42f418a75589378655f757
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-08-14 10:48:20 -0600

    successfully accessed random cocktail from API, render image

--- src/Components/CocktailsPage.js
+++ src/Components/CocktailsPage.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+
 
 const CocktailsPage = () => {
 
+    const [drink, setDrink] = useState("");
+
+    useEffect(() => {
+        axios({
+            url: "https://www.thecocktaildb.com/api/json/v1/1/random.php",
+            method: "GET"
+        }).then(res => {
+            const drinkImage = `${res.data.drinks[0].strDrinkThumb}/preview`;
+            setDrink(drinkImage);
+        })
+    }, []) 
+
     return (
         <div id="cocktails-page">
-
+            <img
+                id="cocktail-image"
+                src={drink}
+                alt="Random photo from CocktailDB"
+            />
+            
         </div>
     )
 }
