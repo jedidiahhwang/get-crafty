bbb1c00f16d475f9218b17069edaaeb4c9f31c10
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-09-06 11:21:38 -0600

    reconfigured homepage image in CSS

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,29 +1,25 @@
 import React from "react";
 
 import "../SASS/_homepage.scss";
-import backgroundImage from "../images/bar-background.jpeg";
+import backgroundImage from "../images/cocktail1.jpg";
 
 const HomePage = () => {
 
     return ( 
         <div id="home-page">
-                <img
+                {/* <img
                     id="background-image"
                     src={backgroundImage}
                     alt="Bar photo from Unsplash"
-                />
+                /> */}
                 <div id="welcome-box">
                     <h1>- Welcome to Get Crafty -</h1>
                     <h3>Your personal bartender</h3>
                 </div>
-                <div id="directions-container">
-                    <p className="intro-text">
-                        Get Crafty is a tool built to solve your cocktail craving needs.<br></br>
-                        Simply select a base liquor as well as any additional ingredients<br></br>
-                        and Get Crafty will generate a list of potential drinks to make!<br></br>
-                    </p>
+                <div id="buttons-container">
+                    <button>Login</button>
+                    <button>Register</button>
                 </div>
-                <div class="pos down-arrow"></div>
         </div>
     )
 }
