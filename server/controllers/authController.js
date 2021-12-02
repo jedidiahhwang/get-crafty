0ed4e12493caffa0559a705164f3fe87788f84bb
Jedidiah <jedidiah.hwang.dev@gmail.com>
2021-12-02 12:33:21 -0700

    successfully put all existing endpoints into controllers

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 const bcrypt = require("bcrypt");
-const mongoose = require("mongoose");
+require("mongoose");
 
 require("../mongoose/mongooseConnect");
 const User = require("../mongoose/mongooseSchema");
@@ -8,6 +8,58 @@ module.exports = {
     getAllUsers: async (req, res) => {
         let results = await User.find({});
         res.status(200).send(results);
+    },
+    login: async (req, res) => {
+        const {email, password} = req.body;
+
+        let results = await User.findOne({email: email});
+      
+        const {_id, firstName, lastName, email: userEmail, recipes} = results;
+        
+        if(Object.keys(results).length === 0) {
+          return res.status(400).send("User does not exist.");
+        };
+      
+        const isAuthenticated = bcrypt.compareSync(password, results.password);
+      
+        if(!isAuthenticated) {
+          return res.status(403).send("Incorrect password");
+        }
+      
+        // Create a new object to return with the password filtered out, so front end doesn't get hash.
+        let returnUser = {
+          _id,
+          firstName,
+          lastName,
+          userEmail,
+          recipes
+        }
+      
+        return res.status(200).send(returnUser);
+    },
+    register: async (req, res) => {
+        const {email, password} = req.body;
+
+        let results = await User.findOne({email: email});
+
+        if(results) {
+            return res.status(409).send("User already exists");
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const passwordHash = bcrypt.hashSync(password, salt);
+
+        req.body.password = passwordHash;
+
+        let myData = new User(req.body);
+        myData.save()
+            .then(() => {
+            res.send("User saved to database");
+            })
+            .catch((err) => {
+            console.log(err);
+            res.status(400).send(err);
+            });
     }
 }
 
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 require("./mongoose/mongooseConnect");
 const User = require("./mongoose/mongooseSchema");
 
-const {getAllUsers} = require("./controllers/authController");
+const {getAllUsers, login, register} = require("./controllers/authController");
 
 const app = express();
 
@@ -21,62 +21,8 @@ app.use(
   })
 );
 
-app.get("/api/user", getAllUsers);
-
-// app.post("/api/login", async (req, res) => {
-//   const {email, password} = req.body;
-
-//   let results = await User.findOne({email: email});
-
-//   const {_id, firstName, lastName, email: userEmail, recipes} = results;
-  
-//   if(Object.keys(results).length === 0) {
-//     return res.status(400).send("User does not exist.");
-//   };
-
-//   const isAuthenticated = bcrypt.compareSync(password, results.password);
-
-//   if(!isAuthenticated) {
-//     return res.status(403).send("Incorrect password");
-//   }
-
-//   // Create a new object to return with the password filtered out, so front end doesn't get hash.
-//   let returnUser = {
-//     _id,
-//     firstName,
-//     lastName,
-//     userEmail,
-//     recipes
-//   }
-
-//   return res.status(200).send(returnUser);
-// });
-
-// app.post("/api/user", async (req, res) => {
-//   const {email, password} = req.body;
-
-//   let results = await User.findOne({email: email});
-
-//   console.log(results);
-
-//   if(results) {
-//     return res.status(409).send("User already exists");
-//   }
-
-//   const salt = bcrypt.genSaltSync(10);
-//   const passwordHash = bcrypt.hashSync(password, salt);
-
-//   req.body.password = passwordHash;
-
-//   let myData = new User(req.body);
-//   myData.save()
-//     .then(() => {
-//       res.send("User saved to database");
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//       res.status(400).send(err);
-//     });
-// });
+app.get("/api/user", getAllUsers); // Dummy endpoint to confirm the server is working
+app.post("/auth/login", login);
+app.post("/auth/user", register);
 
 app.listen(5005, () => console.log("Server is ready on port 5005"));
