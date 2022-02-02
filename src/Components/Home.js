8fe3c09dae8615c34416c54f38e58a7102508f56
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-02-01 21:04:57 -0700

    created and passed props to searchresults

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,29 +6,31 @@ import {Link} from "react-router-dom";
 import {bindActionCreators} from "redux";
 import {actionCreators} from "../redux/actionCreatorExport.js";
 
+import SearchResults from "./SearchResults.js";
+
 import "../SASS/components/_home.scss";
 
-const Home = () => {
+const Home = (props) => {
     const user = useSelector((state) => state.user);
     
-    const [ingredient, setIngredient] = useState("");
+    const [drink, setDrink] = useState("");
+    const [isSearched, setIsSearched] = useState(false);
 
     const handleChange = (event) => {
-        setIngredient(event.target.value);
+        setDrink(event.target.value);
         console.log(event.target.value);
     };
 
     const generateCocktail = (event) => {
         event.preventDefault();
 
-        if(!event.target.value) {
+        if(!drink) {
             console.log("No value input");
         } else {
             axios
-                .get(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
+                .get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
                 .then((res) => {
-                    console.log(res.data);
-                    
+                    setIsSearched(true);
                 })
                 .catch((err) => {
                     console.log(err);
@@ -39,14 +41,19 @@ const Home = () => {
     return ( 
         <div id="home">
             <div id="welcome-box">
-                <h1>Let's make a drink</h1>
-                <div id="form-container">
-                    <form id="ingredient-form" onSubmit={generateCocktail}>
-                        <label id="ingredient-label">
-                            <input id="ingredient-input" type="text" placeholder="Enter a drink name" onChange={handleChange}/>
-                        </label>
-                    </form>
-                </div>
+                
+                {!isSearched ? 
+                    <>
+                        <h1>Let's make a drink</h1>
+                        <div id="form-container">
+                            <form id="ingredient-form" onSubmit={generateCocktail}>
+                                <label id="ingredient-label">
+                                    <input id="ingredient-input" type="text" placeholder="Enter a drink name" onChange={handleChange}/>
+                                </label>
+                            </form>
+                        </div>
+                    </>
+                    : <SearchResults drink={drink} />}
             </div>
         </div>
     )
---
+++ src/Components/SearchResults.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SearchResults = (props) => {
+    return (
+        <div>
+            <h1>You entered {props.drink}</h1>
+        </div>
+    )
+}
+
+export default SearchResults
