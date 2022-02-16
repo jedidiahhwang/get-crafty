03c164958f7b638aa5ad43b8f3921de90da9e521
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-02-16 12:58:22 -0700

    added magnifying glass for search, with functionality

--- src/Components/CocktailSearch.js
+++ src/Components/CocktailSearch.js
@@ -16,6 +16,15 @@ const CocktailSearch = (props) => {
                 <form id="ingredient-form" onSubmit={props.generateCocktail}>
                     <label id="ingredient-label">
                         <input id="ingredient-input" type="text" placeholder="Enter a drink name" onChange={handleChange}/>
+                        <span id="search-button-container">
+                            <button id="search-button">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <circle cx="10" cy="10" r="7"></circle>
+                                    <line x1="21" y1="21" x2="15" y2="15"></line>
+                                </svg>
+                            </button>
+                        </span>
                     </label>
                 </form>
             </div>
